f65cd581d5caf734b5b196f35660e59304c2c6ed
ProfGabote <profgabote@prof.com>
2025-04-27 00:44:00 -0400

    Genera proyecto spring

---
+++ src/main/java/cl/duoc/ejercicios_exp2_solucionario/EjerciciosExp2SolucionarioApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ejercicios_exp2_solucionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosExp2SolucionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosExp2SolucionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ejercicios_exp2_solucionario/EjerciciosExp2SolucionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ejercicios_exp2_solucionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosExp2SolucionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
